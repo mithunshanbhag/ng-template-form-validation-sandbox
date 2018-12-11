764ee726e3849838ba79c87279c2b6ccb1d3a848
mithun shanbhag <mithun@skewcode.com>
2018-12-12 00:05:20 +0530

    Using custom validator in form control

--- src/app/custom-validator.directive.ts
+++ src/app/custom-validator.directive.ts
@@ -1,10 +1,23 @@
 import { Directive } from '@angular/core';
+import { Validator, ValidationErrors, AbstractControl, NG_VALIDATORS } from '@angular/forms';
 
 @Directive({
-  selector: '[appCustomValidator]'
+  selector: '[appCustomValidator]',
+  providers: [{ provide: NG_VALIDATORS, useExisting: CustomValidatorDirective, multi: true }]
 })
-export class CustomValidatorDirective {
+export class CustomValidatorDirective implements Validator {
 
   constructor() { }
 
+  validate(control: AbstractControl): ValidationErrors | null {
+
+    const val = control.value as string;
+    if (!val) {
+      return { 'required': true, 'minlength': true };
+    }
+
+    return (val.length >= 5)
+      ? null
+      : { 'minlength': true };
+  }
 }
